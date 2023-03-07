43302565c6a8f81660573900b4931ee8cd2780af
estelle dupuy <estelledupuy@yahoo.fr>
2023-03-07 14:00:49 +0100

    DO NOT MERGE - test windy

--- app/javascript/controllers/windytest_controller.js
+++ app/javascript/controllers/windytest_controller.js
@@ -2,11 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="windytest"
 export default class extends Controller {
+  static targets = ["wind_u", "wind_v"]
   connect() {
   }
 
-  callApi() {
-    fetch("https://reqres.in/api/register", {
+  callApiWind() {
+    fetch("https://api.windy.com/api/point-forecast/v2", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({
@@ -20,7 +21,29 @@ export default class extends Controller {
     })
     .then(response => response.json())
     .then((data) => {
-      console.log(data)
+      console.log(data);
+      this.wind_uTarget.innerText = data["wind_u-surface"][0];
     })
   }
+
+  // callApiMap() {
+  //   fetch("", {
+  //   method: "POST",
+  //   headers: {"Content-Type": "application/json"},
+  //   body: JSON.stringify({
+  //     "lat": 49.809,
+  //     "lon": 16.787,
+  //     "model": "gfs",
+  //     "parameters": ["wind"],
+  //     "levels": ["surface"],
+  //     "key": "zIF8phEbm4bn3uvRBV8XPXGcnpAX04vL",
+  //     })
+  //   })
+  //   .then(response => response.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //     this.wind_uTarget.text = data["wind_u-surface"][0];
+
+  //   })
+  // }
 }
