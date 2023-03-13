930b80f192856478a1657723a384fc8c5376f908
geoffroy moreaud <g.moreaud@gmail.com>
2023-03-13 18:49:53 +0100

    control ok, menu en cours

---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="toggle"
+export default class extends Controller {
+  static targets = [ "commandes" ];
+
+  initialize() {
+    this.isOpen = false;
+  }
+  connect() {
+    console.log('Hello');
+  }
+
+  menu(){
+    console.log("you clicked the button!");
+    console.log(this.commandesTarget);
+    this.isOpen ? this.hide() : this.show();
+    this.isOpen = !this.isOpen;
+  }
+
+  show() {
+    this.commandesTarget.style.display = "block";
+  }
+
+  hide() {
+    this.commandesTarget.style.display = "none";
+  }
+}
