28acad8ebc7732944eadf494fd386c5e21d64e20
estelle dupuy <estelledupuy@yahoo.fr>
2023-03-15 09:20:12 +0100

    DO NOT MERGE

--- app/javascript/controllers/direction_controller.js
+++ app/javascript/controllers/direction_controller.js
@@ -6,4 +6,40 @@ export default class extends Controller {
 
     console.log('commande controller connected');
   }
+
+  rotateBoat(event) {
+    console.log(event.currentTarget.attributes.id);
+    if (id="a1") {
+      alpha_boat = @wind_dir + 45 + 0
+      }
+    if (id="a2") {
+      alpha_boat = @wind_dir + 45 - 1
+      }
+    if (id="a3") {
+      alpha_boat = @wind_dir + 45 + 59
+      }
+    if (id="a4") {
+      alpha_boat = @wind_dir + 45 + 90
+      }
+    if (id="a5") {
+      alpha_boat = @wind_dir + 45 + 133
+      }
+    if (id="a6") {
+      alpha_boat = @wind_dir + 45 + 178
+      }
+    if (id="a7") {
+      alpha_boat = @wind_dir + 45 + 225
+      }
+    if (id="a8") {
+      alpha_boat = @wind_dir + 45 + 268
+      }
+    if (id="a9") {
+      alpha_boat = @wind_dir + 45 + 300
+      }
+    if (id="a10") {
+      alpha_boat = @wind_dir + 45 + 315
+      }
+    this.boatSelectorTarget.transform = `rotate(${alpha_boat}deg)`
+    // pass on "direction" to update of boat >> via submit form (input.value = ...)
+  }
 }
